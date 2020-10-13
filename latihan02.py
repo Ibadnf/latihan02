ee462ffa1b0cb5ff7859a36a55bfe4e5dd620002
Ibadnf <ibaddilahnf@gmail.com>
2020-10-13 12:00:33 +0700

    update new jam 12

--- latihan02.py
+++ latihan02.py
@@ -1,13 +1,25 @@
+
+number=1+1+2+3/4.0
+print(number)
+
+mod=10%3   # sisa dari pembagian 3
+print(mod)
+
+square=7**2		# perpangkatan
+print(square)
+
+helloworld="Hello"+" "+"World"
+print(helloworld)
+
+myName="Luke"
+print(f"Hello, {myName}")
+
 mylist=["Luke Skywlake","Anakin Skywlaker","Yoda","Padem"]
 
 print(mylist)
 
-mylist=["Luke Skywlake","Anakin Skywlaker"]
-if mylist==2:
-	print('Bener')
-else:
-	print('Salah')
 
+print(mylist[::-1])
 
-mystring=["contoh aja"]
-print(mystring)
+for x in mylist:
+	print(x)
