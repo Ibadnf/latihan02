515ba386eef1a5de7f7669f92c314828817f6e3b
Ibadnf <ibaddilahnf@gmail.com>
2020-10-19 10:31:29 +0700

    Exercise03

---
+++ exercise03.py
@@ -0,0 +1,10 @@
+for x in range(1,11):		# Bisa dibikin simple lagi sebenernya gausah ada continue
+	if x % 2 == 1:
+		continue
+	print(x)
+
+for i in range(1,11):
+	if x % 2 == 0:
+		continue
+	print(i)
+
